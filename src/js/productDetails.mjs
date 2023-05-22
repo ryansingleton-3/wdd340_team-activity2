f9c736c1e314c9821a627266c64d318164e7198a
u060883469 <u060883469@byui.edu>
2023-05-21 19:36:26 -0500

    I moved images and json folder to public folder

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,5 +1,5 @@
-import { findProductById } from "./productData.mjs";
-import { setLocalStorage } from "./utils.mjs";
+import { findProductById } from './productData.mjs';
+import { setLocalStorage } from './utils.mjs';
 
 let product = {};
 
@@ -9,21 +9,21 @@ export default async function productDetails(productId) {
   // once we have the product details we can render out the HTML
   renderProductDetails();
   // once the HTML is rendered we can add a listener to Add to Cart button
-  document.getElementById("addToCart").addEventListener("click", addToCart);
+  document.getElementById('addToCart').addEventListener('addToCart', addToCart);
 }
 function addToCart() {
-  setLocalStorage("so-cart", product);
+  setLocalStorage('so-cart', product);
 }
 function renderProductDetails() {
-  document.querySelector("#productName").innerText = product.Brand.Name;
-  document.querySelector("#productNameWithoutBrand").innerText =
+  document.querySelector('#productName').innerText = product.Brand.Name;
+  document.querySelector('#productNameWithoutBrand').innerText =
     product.NameWithoutBrand;
-  document.querySelector("#productImage").src = product.Image;
-  document.querySelector("#productImage").alt = product.Name;
-  document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
-  document.querySelector("#productColorName").innerText =
+  document.querySelector('#productImage').src = product.Image;
+  document.querySelector('#productImage').alt = product.Name;
+  document.querySelector('#productFinalPrice').innerText = product.FinalPrice;
+  document.querySelector('#productColorName').innerText =
     product.Colors[0].ColorName;
-  document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+  document.querySelector('#productDescriptionHtmlSimple').innerHTML =
     product.DescriptionHtmlSimple;
-  document.querySelector("#addToCart").dataset.id = product.Id;
+  document.querySelector('#addToCart').dataset.id = product.Id;
 }
